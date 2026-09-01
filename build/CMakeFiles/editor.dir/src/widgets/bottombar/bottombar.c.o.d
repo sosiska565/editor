45dd4bc3946b6fa222020369b40ff68723449a0c
sosiska565 <sosifka666@gmail.com>
2026-09-02 00:25:11 +0300

    Delete build directory

--- build/CMakeFiles/editor.dir/src/widgets/bottombar/bottombar.c.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/editor.dir/src/widgets/bottombar/bottombar.c.o: \
- /home/egor/editor/src/widgets/bottombar/bottombar.c \
- /usr/include/stdc-predef.h \
- /home/egor/editor/src/widgets/bottombar/bottombar.h \
- /home/egor/editor/src/widgets/bottombar/../../widget/widget.h
